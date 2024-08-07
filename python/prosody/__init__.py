2fa9884a433a445876497914f1a4eb983ed28c18
Josh Griffith <joshua.griffith@realgeeks.com>
2024-08-07 18:19:18 -0500

    log: remove poll span log spam

--- python/prosody/__init__.py
+++ python/prosody/__init__.py
@@ -1,2 +1,6 @@
+import logging
+
 from .handler import AbstractMessageHandler, TracingHandler
 from .prosody import Context, Message, ProsodyClient
+
+logging.getLogger('prosody.consumer.poll').setLevel(logging.ERROR)
